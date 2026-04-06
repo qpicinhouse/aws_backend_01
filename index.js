07a9eb84131749ba072fef178a97be2b79894b0a
Sovan Ghorai <sovanghorai6@gmail.com>
2026-04-06 23:31:41 +0530

    add one name and dot env

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", profileRoutes);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
