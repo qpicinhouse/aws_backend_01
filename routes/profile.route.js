4e32c45d579b83ea3f9afaf97f7bf0d97ea001c2
Sovan Ghorai <sovanghorai6@gmail.com>
2026-04-08 22:40:16 +0530

    add ok

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getProfile } = require("../controllers/profile.controller.js");
 router.get("/profile", getProfile);
 router.get("/profile-new", (req, res) => {
-  res.send("This is the new profile endpoint 🚀");
+  res.send("This is the new profile endpoint OKKKK 🚀");
 });
 module.exports = router;
